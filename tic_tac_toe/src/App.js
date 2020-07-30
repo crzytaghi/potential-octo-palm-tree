b80c66adfc713e3a07bfbaad4a12fb069ce3f538
Andrew Taghi <aataghi13@gmail.com>
2020-07-30 15:54:30 -0500

    content and style complete

--- tic_tac_toe/src/App.js
+++ tic_tac_toe/src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import Player from './components/Player';
+import Board from './components/Board';
+import Square from './components/Square';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render = () => {
+    return (
+      <div className="app">
+        <Header />
+        <Player whichPlayer="X"/>
+        <Player whichPlayer="O"/>
+        <Board className="board"/>
+      </div>
+    )
+  }
 }
 
 export default App;
